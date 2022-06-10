416b7fbf1b2cb8df184976130b5e0db6fed0668a
yanNotDev <aayantde@hotmail.com>
2022-06-10 12:12:41 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-110px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
